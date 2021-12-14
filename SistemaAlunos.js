5c748091129ae463c0ef29fe86cc92e4ddb32039
Paul1 <ana_mendes@sme.prefeitura.sp.gov.br>
2021-12-13 23:12:01 -0300

    Commit para entrega da tarefa

---
+++ SistemaAlunos.js
@@ -0,0 +1,33 @@
+let nomeDeAluno = ["Luísa", "Rita", "Susana", "Adriana", "Luana"]
+
+for (let nome of nomeDeAluno) {
+   console.log(`Esta pessoa se chama ${nome}`)
+   }
+
+// let numeroDeAlunos =10;
+
+// let contador =0;
+// while(contador <= numeroDeAlunos) {
+//    //console.log(contador)
+
+//       if (contador == 0){
+//       console.log("O número atual é zero")
+//    } else if (contador % 2 == 0){
+//       console.log("O número " + contador +" é par")
+//    }else{
+//       console.log(`O número ${contador} é ímpar`)
+//    }
+//    contador ++;  
+// }
+// for (let contador = 0; contador < numeroDeAlunos; contador++) {
+//    //console.log(contador)
+
+//    console.log(contador)  
+//    if (contador == 0){
+//       console.log("O número atual é zero")
+//    } else if (contador % 2 == 0){
+//       console.log("O número " + contador +" é par")
+//    }else{
+//       console.log(`O número ${contador} é ímpar`)
+//    }
+// }
