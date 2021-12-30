a9807afb7d7875a0c60bdbe7275952426349a7fa
Paul1 <ana_mendes@sme.prefeitura.sp.gov.br>
2021-12-30 20:40:07 -0300

    Tirando comentários do código

--- SistemaAlunos.js
+++ SistemaAlunos.js
@@ -4,30 +4,30 @@ for (let nome of nomeDeAluno) {
    console.log(`Esta pessoa se chama ${nome}`)
    }
 
-// let numeroDeAlunos =10;
+ let numeroDeAlunos =10;
 
-// let contador =0;
-// while(contador <= numeroDeAlunos) {
-//    //console.log(contador)
+let contador =0;
+while(contador <= numeroDeAlunos) {
+       //console.log(contador)
 
-//       if (contador == 0){
-//       console.log("O número atual é zero")
-//    } else if (contador % 2 == 0){
-//       console.log("O número " + contador +" é par")
-//    }else{
-//       console.log(`O número ${contador} é ímpar`)
-//    }
-//    contador ++;  
-// }
-// for (let contador = 0; contador < numeroDeAlunos; contador++) {
-//    //console.log(contador)
+              if (contador == 0){
+       console.log("O número atual é zero")
+    } else if (contador % 2 == 0){
+       console.log("O número " + contador +" é par")
+    }else{
+       console.log(`O número ${contador} é ímpar`)
+    }
+    contador ++;  
+ }
+ for (let contador = 0; contador < numeroDeAlunos; contador++) {
+    //console.log(contador)
 
-//    console.log(contador)  
-//    if (contador == 0){
-//       console.log("O número atual é zero")
-//    } else if (contador % 2 == 0){
-//       console.log("O número " + contador +" é par")
-//    }else{
-//       console.log(`O número ${contador} é ímpar`)
-//    }
-// }
+    console.log(contador)  
+        if (contador == 0){
+       console.log("O número atual é zero")
+    } else if (contador % 2 == 0){
+       console.log("O número " + contador +" é par")
+    }else{
+       console.log(`O número ${contador} é ímpar`)
+    }
+ }
